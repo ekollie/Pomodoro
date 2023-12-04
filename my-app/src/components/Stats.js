e1d544ae065c41ccf001996eb2c7f618255abae9
tylersmith <tyjsmith1@gmail.com>
2023-12-04 11:38:11 -0600

    Component Structure

---
+++ my-app/src/components/Stats.js
@@ -0,0 +1,19 @@
+import React from "react"
+import TotalTime from "./TotalTime"
+import Graph from "./Graph"
+import AdditionalStats from "./AdditionalStats"
+
+
+function Stats () {
+
+
+    return (
+        <div id="stats">
+            <TotalTime />
+            <Graph />
+            <AdditionalStats />
+        </div>
+    )
+}
+
+export default Stats
---
+++ my-app/src/components/TotalTime.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function TotalTime () {
+
+}
+
+export default TotalTime
