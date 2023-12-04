463610bb917d2a08669034f94702c31bb30551a9
Elijah <ekollie324@gmail.com>
2023-12-04 16:48:45 -0500

    added graph

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import Snake from "./components/Snake.js";
-import MainPage from "./components/MainPage.js"
+import MainPage from "./components/MainPage.js";
 
 function App() {
   return (
     <div className="App">
-      <Snake />
+      {/* <Snake /> */}
       <MainPage />
     </div>
   );
--- my-app/src/components/Stats.js
+++ my-app/src/components/Stats.js
@@ -1,19 +1,21 @@
-import React from "react"
-import TotalTime from "./TotalTime"
-import Graph from "./Graph"
-import AdditionalStats from "./AdditionalStats"
+import React from "react";
+import TotalTime from "./TotalTime";
+import Graph from "./Graph";
+import AdditionalStats from "./AdditionalStats";
 
-
-function Stats () {
-
-
-    return (
-        <div id="stats">
-            <TotalTime />
-            <Graph />
-            <AdditionalStats />
-        </div>
-    )
+function Stats() {
+  return (
+    <div id="stats">
+      <h1>Total Time</h1>
+      <TotalTime />
+      <br></br>
+      <h1>Additional Stats</h1>
+      <AdditionalStats />
+      <br></br>
+      <h1>Efficiency Graph</h1>
+      <Graph />
+    </div>
+  );
 }
 
-export default Stats
+export default Stats;
