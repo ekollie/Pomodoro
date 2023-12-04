b12062ecc30c12608e72eab31af49b5af04fc220
tylersmith <tyjsmith1@gmail.com>
2023-12-04 13:12:15 -0600

    text editor pt 2

--- my-app/src/components/TextEditor.js
+++ my-app/src/components/TextEditor.js
@@ -7,6 +7,9 @@ function TextEditor() {
     function handleSubmit (event) {
         event.preventDefault()
         console.log(editorContent)
+        /*POST REQUEST FOR A SEQUENCE + PATCH REQUEST FOR CONTENT UPDATE*/
+        /*CREATE A FUNCTION THAT WILL TRIGGER ROUTE TO GAME*/
+        /*handleSubmit should be invoked by timer expiration*/
     }
 
     function handleChange (event) {
@@ -19,6 +22,7 @@ function TextEditor() {
             <h1>Text Editor</h1>
             <form onSubmit={handleSubmit}>
                 <textarea value={editorContent} onChange={handleChange} rows="30" cols="90" id="project-content" name="project-content"></textarea>
+                {/*REMOVE BUTTON LATER -- REPLACE WITH TIMER*/}
                 <button type="submit">Submit</button>
             </form>
         </div>
