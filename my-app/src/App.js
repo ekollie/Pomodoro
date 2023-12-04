23907885a572a0407bcfc02df23068305e3e8460
tylersmith <tyjsmith1@gmail.com>
2023-12-04 15:18:51 -0600

    timer logic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Snake from "./components/Snake.js";
 import MainPage from "./components/MainPage.js"
 import TextEditor from "./components/TextEditor.js"
+import Timer from "./components/Timer.js"
 
 function App() {
   return (
     <div className="App">
+      <Timer />
       <TextEditor />
       <Snake />
       <MainPage />
