26369bd1fac1b443418442827994fbf7f7acf950
tylersmith <tyjsmith1@gmail.com>
2023-12-06 10:17:54 -0600

    server comms / text editor

--- my-app/src/components/TextEditor.js
+++ my-app/src/components/TextEditor.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom";
 import Timer from "./Timer"
 
@@ -8,38 +8,49 @@ function TextEditor() {
     const [charCount, setCharCount] = useState(0)
     const [totalChars, setTotalChars] = useState(0)
     const [efficiency, setEfficiency] = useState(0)
-    const urls = "http://localhost:3001/projects"
+    const projUrl = "http://localhost:3001/projects"
+    const seqUrl = "http://localhost:3001/sequences"
     const [isActive, setIsActive] = useState(false)
     const navigate = useNavigate()
+    const tempProjId = 1
+
+    useEffect(()=>{
+        fetch(`${projUrl}/${tempProjId}`)
+        .then(res => res.json())
+        .then(proj => setEditorContent(proj.content))
+    },[])
 
     function handleTimerExpiration () {
         handleSubmit()
         /*This activates the route to the snake game once the clock expires*/
+        /*CREATE A FUNCTION THAT WILL TRIGGER ROUTE TO GAME*/
         navigate("/snake")
     }
 
     function handleSubmit() {
         /*POST REQUEST FOR A SEQUENCE + PATCH REQUEST FOR CONTENT UPDATE*/
-        /*CREATE A FUNCTION THAT WILL TRIGGER ROUTE TO GAME*/
-        /*handleSubmit should be invoked by timer expiration*/
+        const newSequence = {
+            id: "",
+            project_id: tempProjId,
+            efficiency: efficiency,
+            duration_seconds: 1500,
+            character_count: charCount,
+            date: "2023-12-6"
+        }
+        fetch(seqUrl, {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify(newSequence)
+        })
+        .then((res)=> {if(res.ok){console.log(res)} else {console.log("POST error")}})
 
-        /*key down counter needs separate state*/
-        /*measuring efficiency by finding difference between total keystrokes & character length of final*/
-
-        // const newProject = {
-        //     id: "",
-        //     project_id: "",
-        //     project_name: "",
-        //     project_category: "",
-        //     project_content: editorContent
-        // }
-
-        fetch(urls, {
+        /*PATCH REQUEST FOR CONTENT UPDATE*/
+        fetch(`${projUrl}/${tempProjId}`, {
             method: "PATCH",
             headers: {"content-type": "application/json"},
-            body: JSON.stringify({project_content: editorContent})
+            body: JSON.stringify({content: editorContent})
         })
-        .then((res)=> {if(res.ok){console.log(res)} else {console.log("error")}})
+        .then((res)=> {if(res.ok){console.log(res)} else {console.log("PATH error")}})
 
     }
 
